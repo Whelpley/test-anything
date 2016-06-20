0c2e26484523b6ff0679219076bbc71abc9dedaf
Whelpley <whelpley@gmail.com>
2016-06-20 14:49:08 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+var emotify = require(process.argv[2]);
+
+
+console.log(emotify(process.argv[3]));
+
+// Try this yourself. We are going to provide the location for the awesome
+// emotify module in process.argv[2] and the String for the test in
+// process.argv[3].
